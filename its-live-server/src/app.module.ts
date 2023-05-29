b4527f4dea9db12e5e087413740916912ce81668
delta-kor <contact@izflix.net>
2023-05-29 16:13:32 +0900

    Fix production options

--- its-live-server/src/app.module.ts
+++ its-live-server/src/app.module.ts
@@ -14,6 +14,7 @@ import * as path from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      playground: process.env.NODE_ENV !== 'production',
     }),
     ArtistModule,
     VideoModule,
